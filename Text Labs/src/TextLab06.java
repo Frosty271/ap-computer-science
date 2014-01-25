184e2ff88de85c9c4ceae3b17ba3b3898e9d3d20
Nick Walker <nick@nickswalker.com>
2014-01-24 23:28:02 -0600

    Update TextLab06
    Add more strategies and benchmarking

--- Text Labs/src/TextLab06.java
+++ Text Labs/src/TextLab06.java
@@ -1,46 +1,155 @@
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TextLab06 {
 	public static void main(String args[]){
+		  SieveOfEratosthenes sieveOfEratosthenes = new SieveOfEratosthenes();
+		  System.out.println("Enter the primes upper bound ===>>") ;
+		  Long input = readLong();
+		  System.out.println("COMPUTING PRIME NUMBERS");
+		  for(long prime: sieveOfEratosthenes.primesUpTo(input)){
+			  System.out.printf("%04d ",prime);
+		  }
 
-		System.out.println("Enter a maximum number:");
-		System.out.println(PrimeHelper.listPrimesUpTo(readInt()));
-
-		System.out.println();
+		  testPrimenessWithAllStrategies(input);
 
 	}
+	//This won't turn out like you expect, probably because Math.sqrt is slower than we'd expect and division is just so fast
+	public static void testPrimenessWithAllStrategies(long input) {
 
-    public static int readInt() {
-        Scanner in = new Scanner(System.in);
-        return in.nextInt();
+		System.out.println("Naive Division:");
+		NaiveDivision naiveDivision = new NaiveDivision();
+		PrimeStrategyBenchmarker naiveDivisionBencher = new PrimeStrategyBenchmarker(naiveDivision);
+		naiveDivisionBencher.executeIsPrime(input);
 
-    }
+		System.out.println("Smart Division:");
+		SmartDivision smartDivision = new SmartDivision();
+		PrimeStrategyBenchmarker smartDivisionBencher = new PrimeStrategyBenchmarker(smartDivision);
+		smartDivisionBencher.executeIsPrime(input);
+
+		System.out.println("Sieve of Eratosthenes:");
+		SieveOfEratosthenes sieveOfEratosthenes = new SieveOfEratosthenes();
+		PrimeStrategyBenchmarker sieveOfEratosthenesBencher = new PrimeStrategyBenchmarker(sieveOfEratosthenes);
+		sieveOfEratosthenesBencher.executeIsPrime(input);
+
+	}
+	public static long readLong() {
+		Scanner in = new Scanner(System.in);
+
+		return in.nextLong();
+
+	}
 }
 
-class PrimeHelper{
-     PrimeHelper(){
-         //AKA don't instantiate me, AKA static class in Java
-         throw new IllegalArgumentException();
-     }
+//STRATEGIES
+interface PrimeStrategy {
+	abstract boolean isPrime(Long input);
+	ArrayList<Long> primesUpTo(long cap);
+}
+class SieveOfEratosthenes implements PrimeStrategy{
+	public boolean isPrime(Long input)
+	{
+		ArrayList<Long> primes = primesUpTo(input);
+		for (Long prime: primes) {
+			if (input.equals(prime)) return true;
+		}
+		return false;
+	}
+	public ArrayList<Long> primesUpTo(long cap)        {
+		//Array indices will limit our search range compared to other strategies.
+		// all are initialized to false  (all are assumed prime)
+		boolean[] composites = new boolean[(int)cap];
 
-	public static boolean isPrime(Integer input)        {
-		double cap = Math.sqrt( (double)input );
+		for(int factor = 2; factor < cap; factor++){
+				for(int i = factor*2; i < cap; i+=factor) composites[i]= true;
+		}
+		//Exceptions
+		composites[0] = composites[1] = true;
+		ArrayList<Long> primeList = new ArrayList<Long>();
+		for (int i = 0; i < composites.length; i++) if(!composites[i]) primeList.add((long)i);
 
-		//If the square root is a whole number, it's not prime.
-		if( cap % 1 == 0 ) return false;
-		for (int i = 2; i < cap; i++) {
-			if( input % i == 0 ) return false;
-			if( (input == i) || input == i-1 ) return false;
+		return primeList;
+
+
+	}
+}
+class SmartDivision implements PrimeStrategy{
+	public boolean isPrime(Long input)
+	{
+		long cap = (long)Math.sqrt(input);
+		for (long test = 2; test < cap; test++) {
+			if ((input % test) == 0) return false;
 		}
 		return true;
 	}
-    public static ArrayList<Integer> listPrimesUpTo(double max)        {
-        ArrayList<Integer> primeList = new ArrayList<Integer>();
-        //Iterate over every number under our cap
-        for (int i = 2; i < max; i++)
+	public ArrayList<Long> primesUpTo(long cap)        {
+		ArrayList<Long> primeList = new ArrayList<Long>();
+		//Iterate over every number under our cap
+		for (long i = 2; i < cap; i++) {
 			if(isPrime(i)) primeList.add(i);
-        return primeList;
-    }
+		}
+		return primeList;
 
+
+	}
+}
+class NaiveDivision implements PrimeStrategy{
+	public boolean isPrime(Long input)        {
+		for (long test = 2; test < input; test++) {
+			if ((input % test) == 0) return false;
+		}
+		return true;
+	}
+	public ArrayList<Long> primesUpTo(long cap)        {
+		ArrayList<Long> primeList = new ArrayList<Long>();
+		//Iterate over every number under our cap
+		for (long i = 2; i < cap; i++) {
+			if(isPrime(i)) primeList.add(i);
+		}
+		return primeList;
+	}
+
+}
+//Helpers
+class PrimeStrategyBenchmarker {
+	private PrimeStrategy strategy;
+	private String timeFormat = "%5.5fms %n";
+	public PrimeStrategyBenchmarker(PrimeStrategy strategy) {
+		this.strategy = strategy;
+	}
+
+	public boolean executeIsPrime(Long input) {
+		ExecutionTimer.benchmark();
+		boolean result = this.strategy.isPrime(input);
+		System.out.printf(timeFormat, (double)ExecutionTimer.benchmark()/1000000);
+		return result;
+	}
+	public ArrayList<Long> executePrimesUpTo(long cap) {
+
+		ExecutionTimer.benchmark();
+		ArrayList<Long> result = this.strategy.primesUpTo(cap);
+		System.out.printf(timeFormat, (double)ExecutionTimer.benchmark()/1000000);
+		return result;
+	}
+}
+//Static class for ease of use.
+class ExecutionTimer{
+	ExecutionTimer(){
+		//AKA don't instantiate me, AKA static class in Java
+		throw new IllegalArgumentException();
+	}
+	static long lastBenchmark;
+	static long benchmark(){
+		long delta = 0;
+		if (lastBenchmark == 0){
+			lastBenchmark = System.nanoTime();
+		}
+		else {
+			delta = System.nanoTime() - lastBenchmark;
+			lastBenchmark = 0;
+		}
+
+		return delta;
+	}
 }
